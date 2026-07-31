96fc6f95e86e89011e4e16e449bbcfcfafcad0b5
Do Dang Minh Luan <dodangminhluan@gmail.com>
2026-07-31 10:38:17 +0700

    feat: add header footer and main layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import DetailPage from "./pages/DetailPage";
-import HomePage from "./pages/HomePage";
-import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="movies/:movieId" element={<DetailPage />} />
         <Route path="about" element={<AboutPage />} />
-        <Route path="home" element={<Navigate to="/" replace />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="movies/:id" element={<DetailPage />} />
       </Route>
     </Routes>
   );
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "./MainLayout.css";
 
 function MainLayout() {
   return (
-    <div className="app-shell">
+    <div className="app-layout">
       <Header />
-      <main className="app-main">
+
+      <main className="main-content">
         <Outlet />
       </main>
+
       <Footer />
     </div>
   );
